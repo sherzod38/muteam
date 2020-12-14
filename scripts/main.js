6e3f56ad2f61716564f0be515889b7228b0b5a4f
Sherzod38 <nadirovsherzod82@gmail.com>
2020-12-14 10:13:01 +0500

    muteam7

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
 var mobil = document.querySelector('#mobil');
 var opened = document.querySelector('.mobil-menu');
 var close = document.querySelector('#close');
+var overlay = document.querySelector('.overlay')
 
 
 mobil.addEventListener('click', function() {
   opened.classList.toggle('open')
+  overlay.classList.toggle('vizible')
+});
+
+overlay.addEventListener('click', function() {
+  overlay.classList.remove('vizible')
+    opened.classList.remove('open')
+
 });
 
 close.addEventListener('click', function() {
   opened.classList.remove('open')
+  overlay.classList.remove('vizible')
+
 });
