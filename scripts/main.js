20b422a6294438699e5473a39890664f0f446d3f
Sherzod38 <nadirovsherzod82@gmail.com>
2020-12-18 21:35:46 +0500

    muteam8

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,25 @@
 var mobil = document.querySelector('#mobil');
 var opened = document.querySelector('.mobil-menu');
 var close = document.querySelector('#close');
-var overlay = document.querySelector('.overlay')
+var overlay = document.querySelector('.overlay');
+
+var titlen = document.querySelector('#spane');
+var titlenm = document.querySelector('#spanem');
+var openem = document.querySelector('.span__inner');
+
+
+titlen.addEventListener('click', function() {
+  // alert('salom');
+  openem.classList.toggle('oven')
+
+});
+titlenm.addEventListener('click', function() {
+  // alert('salom');
+  openem.classList.remove('oven')
+
+});
+
+
 
 
 mobil.addEventListener('click', function() {
